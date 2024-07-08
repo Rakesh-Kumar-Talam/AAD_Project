13af5c5465dd31d45bd19282a63f54ef2861d1b0
ramadarshk <ramadarsh.3545@gmail.com>
2024-07-08 19:32:35 +0530

    small change
    Signed-off-by: ramadarshk <ramadarsh.3545@gmail.com>

--- app/src/main/java/com/example/aad_project/MainActivity3.kt
+++ app/src/main/java/com/example/aad_project/MainActivity3.kt
@@ -14,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ChatPage : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var arraylist: ArrayList<MessageX>
@@ -22,7 +22,7 @@ class ChatPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.chat_page_main)
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v , insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left , systemBars.top , systemBars.right , systemBars.bottom)
