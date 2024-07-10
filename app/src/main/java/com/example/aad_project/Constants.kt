12f76ae62197cda0a2053948f5e3dfe572f4d29d
ramadarshk <ramadarsh.3545@gmail.com>
2024-07-10 13:15:06 +0530

    small change
    Signed-off-by: ramadarshk <ramadarsh.3545@gmail.com>

--- app/src/main/java/com/example/aad_project/Constants.kt
+++ app/src/main/java/com/example/aad_project/Constants.kt
@@ -2,4 +2,4 @@ package com.example.aad_project
 
 const val url="https://chatgpt-api8.p.rapidapi.com/"
 const val x_rapidapi_host="chatgpt-api8.p.rapidapi.com"
-val x_rapidapi_key=BuildConfig.api_key
+val x_rapidapi_key="6ec83efecfmsh53997cf1b4ae405p19b400jsn5befa62ed1ac"
